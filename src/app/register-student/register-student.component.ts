a383c3e58ca7cdb5a1b31c99120cdc6a2b22b339
shawin <shawinpradhan@gmail.com>
2023-09-25 12:56:41 +0530

    SMS

--- src/app/register-student/register-student.component.ts
+++ src/app/register-student/register-student.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Student } from '../student';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-student',
@@ -13,6 +14,24 @@ export class RegisterStudentComponent {
   student: Student = new Student();
 
   constructor(private authService: AuthService, private router: Router) {}
+
+  form = new FormGroup(
+    {
+      name: new FormControl("",[Validators.required, Validators.minLength(2), Validators.maxLength(30)]),
+      email:  new FormControl("",[Validators.required, Validators.email , Validators.maxLength(30)]),
+      phone: new FormControl("",[Validators.required, Validators.minLength(10), Validators.maxLength(10), 
+      Validators.pattern('[6789][0-9]{9}')]),
+      dob: new FormControl("",[Validators.required]),
+      doj: new FormControl("",[Validators.required])
+    }
+  );
+
+  get f()
+  {
+    return this.form.controls;
+  }
+
+
 
   onSubmit()
   {
